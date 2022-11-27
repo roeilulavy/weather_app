0cd9cbdbce7028c5634030c79ce5b621a712d6d1
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-11-27 22:19:03 +0200

    Add Theme

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,41 @@ import Navbar from "./Navbar/Navbar";
 import Home from './Home/Home';
 import Favorites from './Favorites/Favorites';
 import './App.css';
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"));
+    console.log("THEME:" + theme);
+  };
+
   return (
-    <div className="App">
-      <Navbar />
-
-      <Routes>
-        <Route path="*" element={<Navigate to="/home" />} />
-
-        <Route
-              path="/home"
-              element={<Home />}
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <Navbar
+          toggleTheme={toggleTheme}
+          theme={theme}
         />
-        <Route
-              path="/favorites"
-              element={<Favorites />}
-        />
-      </Routes>
-    </div>
+
+        <Routes>
+          <Route path="*" element={<Navigate to="/home" />} />
+
+          <Route
+                path="/home"
+                element={<Home />}
+          />
+          <Route
+                path="/favorites"
+                element={<Favorites />}
+          />
+        </Routes>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Switch from "react-switch";
 import './Navbar.css';
 
-export default function Navbar() {
+export default function Navbar({toggleTheme, theme}) {
+
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+    toggleTheme();
+  };
+
   return (
     <div className='Navbar'>
       <h1 className="Navbar__title">Welcome to your forecast!</h1>
         
       <nav className="Navbar__nav-container">
-        <button className="Navbar__button">Theme</button>
+        <Switch
+          className="Navbar__switch"
+          onChange={handleChange}
+          checked={theme === "dark"}
+        />
         <Link to="/home" className="Navbar__link">Home</Link>
         <Link to="/favorites" className="Navbar__link">Favorites</Link>
       </nav>
