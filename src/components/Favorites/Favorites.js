6dd4fb7b57d9fa7ef13b35cc2c7c308b24a27ca5
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-11-27 18:07:15 +0200

    Add Favorites page

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,9 +1,19 @@
+import ItemFavoriets from '../ItemFavoriets/ItemFavoriets';
 import './Favorites.css';
 
 export default function Favorites() {
   return (
     <div className='Favorites'>
-      Favorites
+      <h1 className='Favorites__title'>Saved places</h1>
+      <div className='Favorites__places-container'>
+        <ItemFavoriets />
+        <ItemFavoriets />
+        <ItemFavoriets />
+        <ItemFavoriets />
+        <ItemFavoriets />
+        <ItemFavoriets />
+        <ItemFavoriets />
+      </div>
     </div>
   );
 }
---
+++ src/components/ItemFavoriets/ItemFavoriets.js
@@ -0,0 +1,16 @@
+import Sun_icon from '../../images/sun.png';
+import './ItemFavoriets.css';
+
+export default function ItemFavoriets() {
+  return (
+    <div className='ItemFavoriets'>
+      <div className='ItemFavoriets-container'>
+        <h1 className='ItemFavoriets__title'>Tel Aviv</h1>
+        <h2 className='ItemFavoriets__tempreture'>38°</h2>
+        <img className='ItemFavoriets__icon' src={Sun_icon} alt='weather icon'/>
+        <h3 className='ItemFavoriets__icon-description'>Sunny</h3>
+      </div>
+      <button className='ItemFavoriets__button'>REMOVE</button>
+    </div>
+  );
+}
