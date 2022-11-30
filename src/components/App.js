56fd81464cd9213c8c59ad59795590b0482b1624
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-11-30 15:41:33 +0200

    Test 9

--- src/components/App.js
+++ src/components/App.js
@@ -98,7 +98,7 @@ function App() {
 
         <Routes>
           <Route
-            path="/"
+            path="/roei-lulavy-27.11.22/home"
             element={
               <Home 
                 isMetric={isMetric}
@@ -110,7 +110,7 @@ function App() {
             }
           />
           <Route
-            path="/favorites"
+            path="/roei-lulavy-27.11.22/favorites"
             element={
               <Favorites 
                 isMetric={isMetric}
@@ -120,7 +120,7 @@ function App() {
               />
             }
           />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/roei-lulavy-27.11.22/home" />} />
         </Routes>
         
       </div>
